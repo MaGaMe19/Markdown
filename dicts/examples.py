698a32e945b1884468e0b4be352f9de043e34f69
MaGaMe19 <62305800+MaGaMe19@users.noreply.github.com>
2020-12-19 22:01:34 +0100

    Beispiele, welche in 08_dicts.md verwendet werden.

---
+++ dicts/examples.py
@@ -0,0 +1,13 @@
+steckbrief = {
+    "Vorname": "Klaus",
+    "Nachname": "Müller",
+    "Wohnort": "Zürich",
+}
+
+zutaten = {
+    1: "Mehl",
+    2: "Eier",
+    3: "Zucker",
+}
+for i, j in zutaten.items():
+    print(f"{i}. {j}")
